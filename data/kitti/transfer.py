1bdf481701f24a7aca3c08baddd302ce2eb41dbe
Gilles Charlier <gilles.charlier97@gmail.com>
2021-03-24 10:24:13 +0100

    data and gitignore updated

---
+++ data/kitti/transfer.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from os import listdir, path
+from os.path import isfile, join
+import fileinput
+
+# on veut que toutes les images qui n'ont pas de labels bougent dans test/images
+dataset_folder = sys.argv[1]
+dataset_train_images_path = dataset_folder+"/train/images"
+dataset_train_labels_path = dataset_folder+"/train/labels"
+
+
+dataset_train_images = [f for f in listdir(dataset_train_images_path) if isfile(join(dataset_train_images_path, f))]
+dataset_train_labels = [f for f in listdir(dataset_train_labels_path) if isfile(join(dataset_train_labels_path, f))]
+
+for train_file in dataset_train_images:
+    if not train_file.split("-")[0]+"-noctuidae.txt" in dataset_train_labels:
+        os.remove(dataset_train_images_path+"/"+train_file)
+
